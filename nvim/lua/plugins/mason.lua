07d62e916ebe55c9a0a79e69d96781d97c0eb580
Jason Decker <curiositypaths@users.noreply.github.com>
2024-06-04 21:06:24 -0400

    Add default list of LSPs and formatters

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "css-lsp",
+      "elixir-ls",
+      "html-lsp",
+      "lua-language-server",
+      "prettier",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server",
+      "yaml-language-server",
+    },
+  },
+}
