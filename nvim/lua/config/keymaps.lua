6d79278442064c1c863b606cb60d413506ce9e3a
Jason Decker <curiositypaths@users.noreply.github.com>
2024-07-04 09:35:27 -0400

    Add temporary fix to move lines in nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Temporary fix to move Lines
+vim.keymap.set("n", "∆", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "˚", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "∆", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "˚", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "∆", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "˚", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
